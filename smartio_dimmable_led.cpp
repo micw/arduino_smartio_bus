43ea9c98a3f4f0f5f69e8257a37965a2d91b6e77
Michael Wyraz <michael@wyraz.de>
2018-11-25 09:46:32 +0100

    Code cleanup for dimmable led, better formula for linear gamma correction

--- smartio_dimmable_led.cpp
+++ smartio_dimmable_led.cpp
@@ -1,59 +1,85 @@
 #include <Arduino.h>
 #include <smartio_dimmable_led.h>
 
+// Arduino dims from 0...255, ESP8266 from 0..1023
+#if defined(ESP8266)
+#define PWM_MAX_VALUE 1023
+#else
+#define PWM_MAX_VALUE 255
+#endif
+
 DimmableLed::DimmableLed(unsigned int pin):
-		pin(pin), pwm_value(1023) {
+		pin(pin), dim_value(1023), old_dim_value(-1), on_off(false) {
 	pinMode(pin,OUTPUT);
 	update();
 }
 
 void DimmableLed::loop() {
-			if (dimming) {
+	if (dimming) {
 		if (millis()-last_dim<3) return; // dim every 3 ms -> 1 dim cycle ~ 3s
 		last_dim=millis();
-		pwm_value+=dim_direction;
-		if (pwm_value<1) { // switch to dim_up
+		dim_value+=dim_direction;
+		if (dim_value<1) {
+			// when dimmed down to minimum, we switch direction an dim up
+			dim_value=1;
 			dim_direction=1;
-			pwm_value=1;
-		} else if (pwm_value>1300) { // max 1300 -> remain ~0.5s at 100%
+		} else if (dim_value>1300) {
+			// Valid dim range ends at 1023. We dimm up to 1300 so
+			// that there's a time window of ~0.5s at 100% before dimming down
+			dim_value=1300;
 			dim_direction=-1;
-			pwm_value=1300;
 		}
 		update();
-			}
-		}
+	}
+}
 
 void DimmableLed::toggle() {
-			on_off=!on_off;
-			update();
-		}
+	on_off=!on_off;
+	update();
+}
 void DimmableLed::dim_start() {
-			dimming=true;
-			if (!on_off) { // off -> dim up
+	dimming=true;
+	if (!on_off) { // off -> dim up
 		dim_direction=1;
-		pwm_value=0;
+		dim_value=0;
 		on_off=true;
-			} else { // continue dim in other direction
+	} else { // continue dim in other direction
 		dim_direction=-dim_direction;
-		if (pwm_value<1) pwm_value=1;
-		else if (pwm_value>1023) pwm_value=1023;
-			}
-		}
+		// dim_value might be >1023 when we was dimming up
+		dim_value=normalize(dim_value,1023);
+	}
+}
 
 void DimmableLed::dim_stop() {
-			dimming=false;
-		}
-		
+	dimming=false;
+}
+
+int DimmableLed::normalize(int value, int max_value) {
+	if (value<0) return 0;
+	if (value>max_value) return max_value;
+	return value;
+}
+
+int DimmableLed::value_to_pwm(int value, int max_value) {
+
+//	return (int)pow(max_value, value/1023.0);
+	// https://www.mikrocontroller.net/articles/LED-Fading
+	// This formular gives an almost linear impression of the fading
+	return (int) (pow(2, log2(max_value) * (value+1)/1024.0) - 1);
+}
+
 void DimmableLed::update() {
-//			Serial.print(dim_direction);
-//			Serial.print(" ");
-//			Serial.println(pwm_value);
-			if (on_off && pwm_value>0) {
-		int v=pwm_value;
-		if (v<0) v=0;
-		else if (v>1023) v=1023;
-		analogWrite(pin,(int)pow(1023, v/1023.0));
-			} else {
-		digitalWrite(pin,0);
-			}
+	if (on_off && dim_value>0) {
+		int v=normalize(dim_value,1023);
+		if (v!=old_dim_value) {
+			// we update the PWM signal only if the actual value changes
+			old_dim_value=v;
+			int pwm_value=value_to_pwm(v,PWM_MAX_VALUE);
+			pwm_value=normalize(pwm_value,PWM_MAX_VALUE);
+			analogWrite(pin,pwm_value);
 		}
+	} else {
+		old_dim_value=-1;
+		digitalWrite(pin,0);
+	}
+}
--- smartio_dimmable_led.h
+++ smartio_dimmable_led.h
@@ -3,6 +3,11 @@
 
 #include <smartio_main_loop.h>
 
+/**
+ * A pwm-dimmable led. Pwm dim range is from 0...1023.
+ * This value is gamma-corrected (so that 511 is ~50% of visible brightness)
+ * and normalized to the device's dim range (0..255 on arduino, 0...1023 on esp8266)
+ */
 class DimmableLed : MainLoopListener {
 	public:
 		DimmableLed(unsigned int pin);
@@ -12,11 +17,15 @@ class DimmableLed : MainLoopListener {
 		void loop();
 	private:
 		unsigned int pin;
-		int pwm_value;
+		int dim_value;
+		int old_dim_value;
 		boolean on_off;
 		unsigned long last_dim=0;
 		short dim_direction=1;
 		boolean dimming;
+
+		int normalize(int value, int max_value);
+		int value_to_pwm(int value, int max_value);
     
     	void update();
 };
