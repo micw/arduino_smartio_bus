714828261d610d95d3005ba3fe1f49052e38bec5
Michael Wyraz <michael@wyraz.de>
2018-12-05 22:01:30 +0100

    Set initial value to ~70%

--- smartio_dimmable_led.h
+++ smartio_dimmable_led.h
@@ -16,7 +16,7 @@ class DimmableLedBase : MainLoopListener {
 		void dim_stop();
 		void loop();
 	protected:
-		int dim_value=1023;
+		int dim_value=700;
 		boolean on_off=false;
 		unsigned long last_dim=0;
 		short dim_direction=1;
@@ -29,12 +29,7 @@ class DimmableLedBase : MainLoopListener {
     	void update();
     	virtual void dim_to(int dim_value)=0;
 
-// Arduino dims from 0...255, ESP8266 from 0..1023
-#if defined(ESP8266)
-    	const int pwm_max_value=1023;
-#else
-    	const int pwm_max_value=255;
-#endif
+    	const int pwm_max_value=PWMRANGE;
 
 };
 
